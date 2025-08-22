e0ee14bd6fef94f3b14c0247bb5c7c4ad01d15de
Alexey <aleksey.aleksandrovich.mc@gmail.com>
2025-08-22 04:10:40 +0300

    fix

--- old_func/get_description_text.py
+++
@@ -1,7 +0,0 @@
-# get_description_text.py
-
-from selenium.webdriver.common.by import By
-
-def get_description_text(description):
-    elements = description.find_elements(By.XPATH, ".//*")
-    return [element.text for element in elements]
--- old_func/get_details_text.py
+++
@@ -1,7 +0,0 @@
-# get_details_text.py
-from selenium.webdriver.common.by import By
-
-def get_details_text(details):
-    elems = details.find_elements(By.XPATH, ".//*")
-    
-    return "\n".join([elem.text for elem in elems])
